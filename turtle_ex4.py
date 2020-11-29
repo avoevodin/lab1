42522ac95059e85d4e840312f3ea8f7e89d80426
Andrey Voevodin <avoevodin8888@gmail.com>
2020-11-29 05:13:28 +0300

    completing of ex4

---
+++ turtle_ex4.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.shape('turtle')
+
+vert_amount = 200
+angle = 180 - (vert_amount - 2) / vert_amount * 180
+edge_width = 1
+
+for i in range(vert_amount):
+	turtle.left(angle)
+	turtle.forward(edge_width)
+	
