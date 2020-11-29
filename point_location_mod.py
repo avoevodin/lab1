d9e989a790916b36968e20f65ba5f6220ebf2e35
Andrey Voevodin <avoevodin8888@gmail.com>
2020-11-29 03:49:13 +0300

    added point_location_mod.py

---
+++ point_location_mod.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+if x > 0 and y > 0:					# x > 0, y > 0
+	print("Первая четверть")
+elif x > 0 and y < 0:			# x > 0, y < 0
+	print("Четвертая четверть")
+elif y > 0:						# x < 0, y > 0
+		print("Вторая четверть")
+else:								# x < 0, y < 0
+		print("Третья четверть")	
