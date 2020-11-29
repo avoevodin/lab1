c489856698a9b7728b98891f034ab609feb90299
Andrey Voevodin <avoevodin8888@gmail.com>
2020-11-29 08:44:31 +0300

    completing of ex8

---
+++ turtle_ex8.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.shape('turtle')
+
+cur_len = 0
+inc_step = 4
+
+for i in range(100):
+	
+	if i == 0:
+		turtle.forward(cur_len)
+		cur_len += inc_step	
+	
+	turtle.left(90)
+	turtle.forward(cur_len)
+	cur_len += inc_step
+	
+	
